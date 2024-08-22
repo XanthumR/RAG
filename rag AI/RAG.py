5681165c744cfdcc618b954ad382e3ac21460da9
Rıdvan Barış Özden <159203563+XanthumR@users.noreply.github.com>
2024-08-22 21:32:59 +0300

    Update RAG.py

--- rag AI/RAG.py
+++ rag AI/RAG.py
@@ -8,17 +8,24 @@ ollama_emb = OllamaEmbeddings(
 new_vector_store = FAISS.load_local(
     "faiss_index", embeddings=ollama_emb, allow_dangerous_deserialization=True
 )
+
+
 def retriever(prompt):
-    prompt1 = prompt.replace(" ", "")
-    results = new_vector_store.similarity_search_with_score(
-        prompt1, k=2
-    )
-    return results[0]
+    scores = {}
+    prompt1 = prompt.split(" ")
+    for word in prompt1:
+        results = new_vector_store.similarity_search_with_score(
+            word, k=1,
+        )
+        scores[results[0][0].page_content] = results[0][1]
+    scores = sorted(scores.items(), key=lambda x: x[1], reverse=False)
+    print(scores)
+    return scores[0][0]
 
 
 model = Ollama(model="llama2")
 while True:
     prompt_now = input('please enter your question')
-    prompt_real = retriever(prompt_now)[0].page_content + "\n"+"reponse according to the data on top and question below\n"+prompt_now
+    prompt_real = "the person asking this question visited this website" + retriever(prompt_now) + "\n" + "reponse according to the data on top and question below\n" + prompt_now
     response_text = model.invoke(prompt_real)
     print(response_text)
